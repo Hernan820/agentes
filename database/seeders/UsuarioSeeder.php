a2d970d6079d8a35e387bcd52be01d277114a2f0
Hernan820 <benitezhernan820@gmail.com>
2022-04-01 11:49:26 -0600

    permission instalado, seed con usuario completo

---
+++ database/seeders/UsuarioSeeder.php
@@ -0,0 +1,35 @@
+<?php
+
+use Faker\Factory as Faker;
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+use Spatie\Permission\Models\Role;
+use Spatie\Permission\Models\Permission;
+use Illuminate\Support\Facades\Hash;
+use App\Models\User;
+
+
+class UsuarioSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+
+     $administrador = Role::create(['name' => 'administrador']);
+     $agente = Role::create(['name' => 'agente']);
+
+   $user = User::create([
+         'name' => 'admin',
+         'email' => 'admin@gmail.com',
+         'password' => Hash::make('admin123')
+
+     ]);
+     $user->assignRole('administrador'); 
+
+    }
+}
